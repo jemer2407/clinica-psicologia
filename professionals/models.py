bbbc060ea06a8f378b42e539c09d2f1b6d45f72d
jemer2407 <jemer2407@gmail.com>
2024-11-20 06:53:02 +0100

    update

--- professionals/models.py
+++ professionals/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from specialtys.models import Specialty
 
+def custom_upload_to(instance, filename):
+    old_instance = Professional.objects.get(pk=instance.pk)
+    old_instance.avatar.delete()
+    return 'profile/' + filename
 
 # Create your models here.
 
@@ -8,7 +12,7 @@ class Professional(models.Model):
     name = models.CharField(max_length=50, verbose_name='Nombre')
     surnames = models.CharField(max_length=100, verbose_name='Apellidos')
     specialty = models.ManyToManyField(Specialty, verbose_name='Especialidad')
-    image = models.ImageField(verbose_name='Imagen', upload_to='professionals')
+    image = models.ImageField(verbose_name='Imagen', upload_to=custom_upload_to)
     degree = models.CharField(max_length=100, verbose_name='Titulación')
     resume = models.TextField(verbose_name='Curriculum')
 
--- specialtys/models.py
+++ specialtys/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 
+def custom_upload_to(instance, filename):
+    old_instance = Specialty.objects.get(pk=instance.pk)
+    old_instance.avatar.delete()
+    return 'profile/' + filename
+
 # Create your models here.
 
 class Specialty(models.Model):
     name = models.CharField(max_length=200, verbose_name='Nombre')
     description = models.TextField(verbose_name='Descripción', null=True, blank=True)
-    image = models.ImageField(verbose_name='Imagen', upload_to='specialty', null=True, blank=True)
+    image = models.ImageField(verbose_name='Imagen', upload_to=custom_upload_to, null=True, blank=True)
 
     class Meta:
         verbose_name = 'Especialidad'
